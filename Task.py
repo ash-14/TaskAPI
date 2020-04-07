fd0f47681cbc73d990f2a27ed2f967fc8fbf1350
ash-14 <59388302+ash-14@users.noreply.github.com>
2020-04-07 18:40:06 +0530

    Create Task.py

---
+++ Task.py
@@ -0,0 +1,31 @@
+class Task:
+    def __init__(self, taskId, taskTitle, taskDueDate, taskStatus):
+        self.taskId = taskId
+        self.taskTitle = taskTitle
+        self.taskDueDate = taskDueDate
+        self.taskStatus = taskStatus
+
+    def getTaskId(self):
+        return self.taskId
+    
+    def getTaskTitle(self):
+        return self.taskTitle
+    
+    def getTaskDueDate(self):
+        return self.taskDueDate
+    
+    def getTaskStatus(self):
+        return self.taskStatus
+    
+    def setTaskId(self, taskId):
+        self.taskId = taskId
+    
+    def setTaskDueDate(self, taskDueDate):
+        self.taskDueDate = taskDueDate
+    
+    def setTaskTitle(self, taskTitle):
+        self.taskStatus = taskStatus
+        
+    def setTaskStatus(self, taskStatus):
+        self.taskStatus = taskStatus
+    
