3d37244053b682b68926d1b43a0f7e2d569f527f
ash-14 <59388302+ash-14@users.noreply.github.com>
2020-04-07 17:07:41 +0530

    Create AuthenticationSystem.py

---
+++ AuthenticationSystem.py
@@ -0,0 +1,20 @@
+import hashlib
+
+class AuthenticationSystem:
+    def __init__(self):
+        self.__storageSystemClient = new StorageSystem();
+        
+    def authenticateUser(self, username, password):
+        authenticationMap = self.__storageSystemClient.loadAuthenticationFile();
+        if username in authenticationMap and authenticationMap[username] == password:
+            return True
+        return False
+    
+    def addUserCredentials(self, username, password):
+        authenticationMap = self.__storageSystemClient.loadAuthenticationFile();
+        if username not in authenticationMap:
+            authenticationMap[username] = password
+            self.__storageSystemClient.updateAuthenticationFile(authenticationMap)
+            return True
+        
+        return False
