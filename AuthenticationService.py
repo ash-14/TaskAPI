23770573425d5b3d4a09243334b7a07011c8bd05
ash-14 <59388302+ash-14@users.noreply.github.com>
2020-04-07 19:56:54 +0530

    Update AuthenticationService.py

--- AuthenticationService.py
+++ AuthenticationService.py
@@ -2,19 +2,19 @@ import hashlib
 
 class AuthenticationService:
     def __init__(self):
-        self.__storageSystemClient = new StorageSystem();
+        self.storageSystemClient = new StorageSystem();
         
     def authenticateUser(self, username, password):
-        authenticationMap = self.__storageSystemClient.loadAuthenticationFile();
+        authenticationMap = self.storageSystemClient.loadAuthenticationFile();
         if username in authenticationMap and authenticationMap[username] == password:
             return True
         return False
     
     def addUserCredentials(self, username, password):
-        authenticationMap = self.__storageSystemClient.loadAuthenticationFile();
+        authenticationMap = self.storageSystemClient.loadAuthenticationFile();
         if username not in authenticationMap:
             authenticationMap[username] = password
-            self.__storageSystemClient.updateAuthenticationFile(authenticationMap)
+            self.storageSystemClient.updateAuthenticationFile(authenticationMap)
+            self.storageSystemClient.addUserSpace(username)
             return True
-        
         return False
